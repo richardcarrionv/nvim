e0fbd7a4a152ba4892d813d454c7de790e5aa4fd
Richard Carrion <noesrichard@hotmail.com>
2023-07-26 17:19:58 -0500

    build_dir to out_dir

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,7 +1,7 @@
 vim.g.vimtex_view_method              = 'mupdf'
 vim.g.vimtex_quickfix_mode            = 0
 vim.g.vimtex_compiler_latexmk         = {
-  build_dir = 'out',
+  out_dir = 'out',
 }
 vim.g.vimtex_compiler_latexmk_engines = {
   luatex = '-pdf -pdflatex=luatex',
