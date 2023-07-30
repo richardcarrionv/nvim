e21ea701f2d86301e4e5fb215ddf118c5fe7b791
Richard Carrion <noesrichard@hotmail.com>
2023-07-30 16:20:07 -0500

    Custom snippets and tab completion

--- lua/plugins/lsp/luasnip.lua
+++ lua/plugins/lsp/luasnip.lua
@@ -2,6 +2,10 @@ require('luasnip').setup({})
 
 local fn = vim.fn
 local friendly_snippets = fn.stdpath('data') .. '/site/pack/packer/start/friendly-snippets'
+local my_snippets = '~/.config/nvim/snippets/'
 
 --.local/share/nvim/site/pack/packer/start/friendly-snippets/snippets/
-require("luasnip/loaders/from_vscode").load({ paths = {friendly_snippets} })
+require("luasnip/loaders/from_vscode").load({ paths = {friendly_snippets, my_snippets} })
+
+require('luasnip').filetype_extend("javascript", { "javascriptreact" })
+require('luasnip').filetype_extend("javascript", { "html" })
--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -25,25 +25,25 @@ local mappings = {
   ['<Enter>'] = cmp.mapping.confirm({ select = true }),
   ['<C-space>'] = cmp.mapping.complete(),
   ['<C-o>'] = cmp.mapping.complete(),
-  ['<C-.>'] = cmp.mapping(function(fallback)
-    if luasnip.expand_or_jumpable() then
-      luasnip.expand_or_jump()
-    else
-      fallback()
-    end
-  end, { "i", "s" }),
   ["<Tab>"] = cmp.mapping(function(fallback)
     if cmp.visible() then
       cmp.select_next_item()
+      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+      -- they way you will only jump inside the snippet region
+    elseif luasnip.expand_or_jumpable() then
+      luasnip.expand_or_jump()
     elseif has_words_before() then
       cmp.complete()
     else
       fallback()
     end
   end, { "i", "s" }),
+
   ["<S-Tab>"] = cmp.mapping(function(fallback)
     if cmp.visible() then
       cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+      luasnip.jump(-1)
     else
       fallback()
     end
@@ -54,8 +54,8 @@ local mappings = {
 cmp.setup({
   mapping = cmp.mapping.preset.insert(mappings),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp',      max_item_count = 5 },
-    { name = 'luasnip',       max_item_count = 1 },
+    { name = 'nvim_lsp',      max_item_count = 10 },
+    { name = 'luasnip',       max_item_count = 5 },
     { name = 'path',          max_item_count = 3 },
     { name = 'latex_symbols', max_item_count = 3 },
   }),
